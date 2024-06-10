cb020f0b7102cf713ba2be3087e3c6bee7eabfaf
Antonio Di Dia <hostingloginstudio@gmail.com>
2024-06-10 14:41:10 +0200

    Add files via upload

---
+++ js/aboutus.js
@@ -0,0 +1,20 @@
+﻿function isElementCreated(elId, callback) {
+    const intervalId = setInterval(() => {
+        if (document.getElementById(elId)) {
+            clearInterval(intervalId);
+            callback(true);
+        }
+    }, 200);
+}
+
+isElementCreated('parameterExample', (result) => {
+	if (result) {
+
+		var url = window.location.href;
+		var value1 = new URL(url).searchParams.get('value1');
+		var value2 = new URL(url).searchParams.get('value2');
+		if (value1.length > 0) {
+			document.getElementById("parameterExample").innerHTML = "<b>Parameters URL: value1=" + value1 + "; " + "value2=" + value2 + "</b>";
+		}
+	}
+});
---
+++ js/plainspa/0.9.2/plainspa.js
@@ -0,0 +1,190 @@
+// Copyright (c) Antonio Di Dia. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
+var indexFileName = "index.html";
+var pagesFileExtension = ".html";
+
+// on first load or refresh, it analyzes the browser URL
+
+var requestPage = plainspaGetPath();
+var parameterQuery = getUrlParameters(window.location);
+
+if (requestPage.length == 0 || requestPage == indexFileName) {
+	requestPage = "home";
+}
+
+if (parameterQuery == "?") {
+	parameterQuery = "";
+}
+
+plainspaNavigateTo(requestPage, parameterQuery);
+
+// return the path after the domain name
+function plainspaGetPath() {
+	var urlCompleto = window.location.pathname;
+
+	if (urlCompleto.charAt(0) === '/') {
+		urlCompleto = urlCompleto.slice(1);
+	}
+
+	return urlCompleto;
+}
+
+// get url parameters in a string
+function getUrlParameters(url) {
+	const params = new URLSearchParams(url.search);
+	const paramArray = Array.from(params.entries());
+	const paramString = paramArray.map(([key, value]) => `${key}=${value}`).join('&');
+	return `?${paramString}`;
+}
+
+// page change management when you press the "Back" or "Forward" button in the browser
+window.addEventListener('popstate', function (event) {
+	if (event.state && event.state.page) {
+		plainspaLoadPage(event.state.page, parameterQuery);
+	}
+});
+
+function plainspaNavigateTo(page, pQuery = '') {
+	plainspaLoadPage(page, pQuery);
+
+	// simulate the page change and update the URL
+	var pageBar = page;
+	if (pageBar == "home") { pageBar = ""; }
+	window.history.pushState({ page: page }, null, `/` + pageBar + pQuery);
+}
+
+// load the contents of the page
+function plainspaLoadPage(page, pQuery) {
+	const contentDiv = document.getElementById('plainspa-content');
+	contentDiv.innerHTML = '';
+
+	plainspaReadHtmlFile(page, pQuery)
+		.then(content => { contentDiv.innerHTML = content; })
+		.catch(error => { contentDiv.innerHTML = "page loading error"; });
+}
+
+function plainspaAddProgressBar() {
+	var progressBar = document.createElement('div');
+	progressBar.id = 'plainspa-progress-bar';
+	document.body.appendChild(progressBar);
+}
+
+function plainspaReadHtmlFile(fileName, pQuery) {
+	return new Promise((resolve, reject) => {
+		var xhr = new XMLHttpRequest();
+
+		plainspaAddProgressBar();
+		var progressBar = document.getElementById('plainspa-progress-bar');
+
+		xhr.open('GET', '/pages/' + fileName + pagesFileExtension + pQuery, true);
+
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 1) {
+				progressBar.style.width = '0%';
+			} else if (xhr.readyState === 3) {
+				progressBar.style.width = '50%';
+			} else if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					progressBar.style.width = '100%';
+					setTimeout(() => {
+						progressBar.remove();
+					}, 500);
+					plainspaLaunchJs(xhr.responseText);
+					var result = plainspaExtractHtmlElements(xhr.responseText);
+					plainspaUpdateTitle(result.title);
+					plainspaUpdateMetaDescription(result.metaDescription);
+					resolve(result.styles + result.body);
+				} else {
+					progressBar.remove();
+					reject(new Error(xhr.statusText));
+				}
+			}
+		};
+
+		xhr.send();
+	});
+}
+
+// extract all the js and add them to the document
+function plainspaLaunchJs(htmlContent) {
+	var tempHtml = document.createElement('div');
+	tempHtml.innerHTML = htmlContent;
+
+	var scripts = tempHtml.getElementsByTagName('script');
+	for (var i = 0; i < scripts.length; i++) {
+		var script = document.createElement('script');
+		if (scripts[i].src) {
+			script.src = scripts[i].src;
+		} else {
+			script.text = scripts[i].text;
+		}
+		document.body.appendChild(script);
+	}
+}
+
+// extract some html element
+function plainspaExtractHtmlElements(htmlContent) {
+	var tempHtml = document.createElement('html');
+	tempHtml.innerHTML = htmlContent;
+
+	var bodyContent = '';
+	var bodyTag = tempHtml.getElementsByTagName('body')[0];
+	if (bodyTag) {
+		bodyContent = bodyTag.innerHTML;
+	}
+
+	var stylesContent = '';
+	var styleTags = tempHtml.getElementsByTagName('style');
+	for (var i = 0; i < styleTags.length; i++) {
+		stylesContent += styleTags[i].outerHTML;
+	}
+
+	var metaDescriptionContent = '';
+	var metaTags = tempHtml.getElementsByTagName('meta');
+	for (var i = 0; i < metaTags.length; i++) {
+		if (metaTags[i].getAttribute('name') === 'description') {
+			metaDescriptionContent = metaTags[i].getAttribute('content');
+			break;
+		}
+	}
+
+	var titleContent = '';
+	var titleTag = tempHtml.getElementsByTagName('title')[0];
+	if (titleTag) {
+		titleContent = titleTag.innerHTML;
+	}
+
+	return {
+		body: bodyContent,
+		styles: stylesContent,
+		metaDescription: metaDescriptionContent,
+		title: titleContent
+	};
+}
+
+// update the page Title
+function plainspaUpdateTitle(newTitle) {
+	if (typeof newTitle !== 'string') {
+		return;
+	}
+
+	document.title = newTitle;
+}
+
+// update the description page meta tag
+function plainspaUpdateMetaDescription(newDescription) {
+	if (typeof newDescription !== 'string') {
+		return;
+	}
+
+	const metaDescription = document.querySelector('meta[name="description"]');
+
+	if (metaDescription) {
+		metaDescription.setAttribute('content', newDescription);
+	} else {
+		const newMeta = document.createElement('meta');
+		newMeta.name = 'description';
+		newMeta.content = newDescription;
+		document.head.appendChild(newMeta);
+	}
+}
