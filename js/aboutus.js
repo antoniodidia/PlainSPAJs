5665baf9dd83393274c75a7f0400bbd904940060
Antonio Di Dia <hostingloginstudio@gmail.com>
2024-06-18 13:08:40 +0200

    Update aboutus.js

--- js/aboutus.js
+++ js/aboutus.js
@@ -13,8 +13,8 @@ isElementCreated('parameterExample', (result) => {
 		var url = window.location.href;
 		var value1 = new URL(url).searchParams.get('value1');
 		var value2 = new URL(url).searchParams.get('value2');
-		if (value1.length > 0) {
-            document.getElementById("parameterExample").innerHTML = "<b>Parameters URL: value1=" + value1 + "; " + "value2=" + value2 + "</b>";
+		if (value1 != null && value2 != null && value1.length > 0 && value2.length > 0) {
+			document.getElementById("parameterExample").innerHTML = "<b>Parameters URL: value1=" + value1 + "; " + "value2=" + value2 + "</b>";
 		}
 	}
 });
